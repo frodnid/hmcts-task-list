de1dfdca37cdc49f948852a92ef877e2a977879f
Code <james-code@MacBookPro.5gee.router>
2025-09-16 15:06:41 +0100

    organise seed data

--- backend-api/db/seed.js
+++ backend-api/db/seed.js
@@ -1,7 +1,8 @@
 const { db } = require("./connection");
+const { tasksData } = require("./data");
+const format = require("pg-format");
 
 exports.seed = function() {
-
     return db.query("DROP TABLE IF EXISTS tasks;")
         .then(()=> {
             console.log("dropped")
@@ -10,19 +11,17 @@ exports.seed = function() {
                     task_id SERIAL PRIMARY KEY,
                     title VARCHAR NOT NULL,
                     description VARCHAR DEFAULT NULL,
-                    status VARCHAR CHECK (status = 'TODO' OR status = 'COMPLETED'),
+                    status VARCHAR CHECK (status = 'TODO' OR status = 'COMPLETED' OR status = 'IN PROGRESS'),
                     due_date DATE NOT NULL 
                     ); `)
         }).then(()=> {
             console.log("created")
-            return db.query(`
-                INSERT INTO tasks (title, description, status, due_date)
-                VALUES 
-                ('Database', 'create a db that works with node', 'TODO', '2025-09-20'),
-                ('Testing', 'create unit tests', 'TODO', '2025-09-20'),
-                ('Meal', 'eat breakfast', 'COMPLETED', '2025-09-16'),
-                ('Walk', DEFAULT, 'COMPLETED', '2025-09-16');`)
+            console.log(format("INSERT INTO tasks(title, description, status, due_date) VALUES %L",
+                tasksData.map((JSON) => Object.values(JSON))
+            ))
+            return db.query(format("INSERT INTO tasks(title, description, status, due_date) VALUES %L",
+                tasksData.map((JSON) => Object.values(JSON))
+            ));
         })
-
 
 }
