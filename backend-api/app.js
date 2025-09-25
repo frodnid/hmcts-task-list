4244dee30c2c6d56bcc3e81610c37807a7e68917
Code <james-code@MacBookPro.5gee.router>
2025-09-25 14:18:53 +0100

    add api documentation

--- backend-api/app.js
+++ backend-api/app.js
@@ -19,6 +19,12 @@ const app = express();
 app.use(cors())
 app.use(express.json());
 
+app.get("/api", (req, res) => {
+  res.sendFile("view.html", {
+    root: __dirname
+  });
+})
+
 app
   .route("/api/tasks")
   .get(getTasks)
